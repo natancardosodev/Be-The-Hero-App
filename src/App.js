21b91184d8bb0ca57babc7bbc739c3438bc430ae
Natan Cardoso <natancb7@gmail.com>
2020-03-28 16:32:07 -0300

    Criando componentes de teste App e Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import Header from './Header';
+
 function App() {
+  let counter = 0;
+
+  function increment() {
+    counter += 1;
+
+    console.log(counter); // Variavel é alterada, mas não refletida, por causa do conceito de Estado
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
